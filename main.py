3500cd6d3af8d98c75e0290f5a6d2ec296a4d9f3
george <georget415@gmail.com>
2021-01-11 11:29:33 +1100

    * minor fixes in save_database_list

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ import PresentData as PresentData
 
 with open('credentials.txt', 'r') as f:
     CREDENTIALS = f.readline().split(',')
+#browser = FTPUtils.login(CREDENTIALS)
 
-browser = FTPUtils.login(CREDENTIALS)
-#x = PlayerDatabase.player_search(search_type='transfer_market', use_browser=browser)
