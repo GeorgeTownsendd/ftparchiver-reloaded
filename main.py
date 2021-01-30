f4912ce62176ed9357a2680ca9ea4f0b3c265f11
george <georgeT415@gmail.com>
2021-01-30 18:57:45 +1100

    * georgePC move changes

--- main.py
+++ main.py
@@ -233,7 +233,7 @@ def create_advanced_table(round_n):
 
 if __name__ == '__main__':
     #PresentData.save_team_training_graphs(4791, 'teams-weekly')
-    #unique_database_list = list(set(['market-archive', 'nzl-od-34', 'u16-weekly', 'u21-national-squads', 'teams-weekly', 'nzl-t20-33', 'sa-od-42', 'PGT', 'u21-national-squads']))
+    unique_database_list = list(set(['nzl-od-34', 'u16-weekly', 'u21-national-squads', 'teams-weekly', 'nzl-t20-33', 'sa-od-42', 'PGT', 'u21-national-squads']))#', market-archive']))
     #for db in unique_database_list:
     #    PlayerDatabase.download_database(db)
     #db = PlayerDatabase.watch_database_list(unique_database_list)
@@ -241,5 +241,5 @@ if __name__ == '__main__':
     #database_name = 'teams-weekly'
     #training_data = FTPUtils.catagorise_training('all')
 
-    curr_round_table = create_advanced_table(2)
-
+    #curr_round_table = create_advanced_table(4)
+    pass
