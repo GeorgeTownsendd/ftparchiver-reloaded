f1fc5377c5ebb8f84e428c55912dec1208d7dfcd
george <georget415@gmail.com>
2021-02-08 13:20:51 +1100

    * corrected main for production branch

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import re
 if __name__ == '__main__':
     #PlayerDatabase.download_database('market-archive')
     #PresentData.save_team_training_graphs(4791, 'teams-weekly')
-    unique_database_list = ['teams-weekly']#list(set(['nzl-od-34', 'u16-weekly', 'market-archive', 'u21-national-squads', 'teams-weekly', 'nzl-t20-33', 'sa-od-42', 'PGT', 'u21-national-squads']))
+    unique_database_list = list(set(['nzl-od-34', 'u16-weekly', 'market-archive', 'u21-national-squads', 'teams-weekly', 'nzl-t20-33', 'sa-od-42', 'PGT', 'u21-national-squads']))
     dbstack = PlayerDatabase.watch_database_list(unique_database_list)
     #for db in unique_database_list:
     #    PlayerDatabase.download_database(db)
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     #database_name = 'teams-weekly'
     #training_data = FTPUtils.catagorise_training('all')
 
-    next_database_download = dbstack[0]
-    PlayerDatabase.download_database(next_database_download[1], download_teams_whitelist=next_database_download[2], age_override=next_database_download[3])
+    #next_database_download = dbstack[0]
+    #PlayerDatabase.download_database(next_database_download[1], download_teams_whitelist=next_database_download[2], age_override=next_database_download[3])
